5bb64651a697780c3c5849261575caa77ca72ed3
Austin Murad <60935619+amurad7@users.noreply.github.com>
2020-03-27 19:57:44 -0400

    Here is the new file with the time plot of each algorithm

--- lab8/gh9591_gr0034_Lab#8.cpp
+++ lab8/gh9591_gr0034_Lab#8.cpp
@@ -1,125 +1,326 @@
 //==========================================================
 //
-// Title:      Fundamental Structures Lab 8
+// Title:      Lab 8
 // Course:     CSC 1501
-// Lab Number: Lab #8
+// Lab Number: Lab 8
 // Author:     Austin Murad, Timothy Armstrong
-// Date:       03/06/2020
+// Date:       3/10/2020
 // Description:
-//   This program will run through different sorting
-// algorithms
+//   this program will
+// - test different sorting algorithms
+//
+//
 //==========================================================
-#include <cstdlib> //For several general-purpose functions
-#include <iomanip> //For formatted output
-#include <iostream> //For cin, cout, and system
-#include <string> //For string data type
-#include <cmath> //For several math functions
-#include <fstream> //For file handling
-using namespace std;
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+#include <stdio.h>
+#include <cmath>
+#include <time.h>
+using namespace std; // So "std::cout" may be abbreviated to "cout"
 
-//==========================================================
-//Global
-//==========================================================
-int arrSize = 6;
-
-//==========================================================
-//create a class of Sort
-//==========================================================
+// class decalration
 class Sort
 {
 public:
-    void randNumGen(int arr[]);
-    //Will generate the random number of either 10, 100, 1000, 10000 or 100000
     
-    void selectionSort(int arr[]);
-    //
+    //Generate random integers
+    void randomIntegers(int size);
+
+    //function to swap to elements
+    void swap(int* p_1, int* p_2);
+
+    //Function for Select Sort
+    //input: arr = array of numbers, l = size of the array
+    //output: run time and sorted array
+    void selectSort(int arr[], int l);
     
-    void quickSort(int arr[]);
-    //
-    
-    void mergeSort(int arr[]);
-    //
+    //Partition function
+    //takes the last element as pivot then moving all the
+    //#'s > pivot to the right and #'s < pivot to the left
+    int partition(int arr[], int first, int last);
+
+
+    //Function for Quick Sort
+    //input: arr = array of numbers to be sorted, first = starting index
+    //       last = ending index
+    //output: run time and sorted array
+    void quickSort(int arr[],int first, int last);
+
+    //Function for dividing the input array in half then merges the two halves into an arrays
+    void merge(int arr[], int l, int m, int r);
+
+    //Function for merge sort
+    //input:arr[] = arr of numbers, int l = left, int r = right
+    //output: sorted array and the run time
+    void mergeSort(int arr[], int l, int r);
+
+private:
+    int arr[4];
 };
 
-//==========================================================
-//implement the randNumGen
-//==========================================================
-void Sort::randNumGen(int arr[])
+//function to provide the random intergers
+//input: size = number emements in the array
+//output:
+void Sort::randomIntegers(int size)
 {
-    //to generate random see for rand()
-    srand(time(0));
+    int arr[5];
+    int j = 0;
+    int k = 1;
     
-    //loop through the array
-    for (int i = 0; i < arrSize; i++)
+    for (int i = 0; i <= size - 1; i++)
     {
-        //randomize an idex number in the range of 6
-        int index = rand() % 6;
-        //assign a value of the random index to a temperary variable then equat the numbers to the reandom index given in the array to get 1 of each of the elements in the array
-        swap(arr[i], arr[index]);
+        j = pow(10, k);
+        arr[i] = rand() % j + 1;
+        k++;
     }
-    
-    //output the array, might not use...
-    for (int i = 0; i < arrSize; i++)
+
+    for (k = 0; k <= size - 1; k++)
     {
-        cout << arr[i] << endl;
+        cout << arr[k] << " ";
     }
+    cout << endl;
 }
 
-//==========================================================
-//implement selectionSort
-//==========================================================
-//look over this algorithm and make sure it is what he wants in the pseudo code given
-void Sort::selectionSort(int arr[])
+//Swap function for selcet sort
+//swap the values
+void Sort::swap(int* a, int* b)
 {
-    //declare variable
-    int minIndex;
-    //loop to find index
-    for (int i = 0; i < arrSize - 1; i++)
+    int save = *a;
+    *a = *b;
+    *b = save;
+}
+
+//function for solect sort
+//input: arr = array of numbers, l = length of the array
+//output: run time and sorted array
+void Sort::selectSort(int arr[], int l)
+{
+    int minVal = 0;
+    Sort s;
+
+    //for loop moveing through the unsorted subarray
+    for (int u = 0; u < l - 1; u++)
     {
-        minIndex = i;
-        for (int j = i + 1; j < arrSize; j++)
+        minVal = u;
+
+        //for loop to find the smallest element
+        for (int c = u + 1; c < l; c++)
         {
-            if (arr[j] < arr[minIndex])
-            {
-                minIndex = j;
-            }
-            swap(arr[i], arr[minIndex]);
+            if (arr[c] < arr[minVal])
+                minVal = c;
+        }
+        //call swap function to swap the elements
+        s.swap(&arr[minVal], &arr[u]);
+    }
+
+    //for loop to print out the array
+    for (int i = 0; i <= l-1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+//Partition function
+//takes the last element as pivot then moving all the
+//#'s > pivot to the right and #'s < pivot to the left
+int Sort::partition(int arr[], int first, int last)
+{
+    //decalre variables
+    int pivot = arr[last];
+    int i = (first - 1);
+    Sort s;
+
+    //for loop for looping throught the array
+    for (int j = first; j <= last - 1; j++)
+    {
+        //swaps if current element is smaller then pivot
+        if (arr[j] < pivot)
+        {
+            i++;
+            s.swap(&arr[i], &arr[j]);
         }
     }
+    s.swap(&arr[i + 1], &arr[last]);
+    
+    
+    return(i + 1);
 }
 
-//==========================================================
-//implement quickSort
-//==========================================================
-void Sort::quickSort(int arr[])
+//Function for the quick sort
+//input: arr = array of numbers to be sorted, first = starting index
+//       last = ending index
+//output: run time and sorted array
+void Sort::quickSort(int arr[], int first, int last)
 {
-    cout << "working" << endl;
-}
-
-//==========================================================
-//implement mergeSort
-//==========================================================
-void Sort::mergeSort(int arr[])
-{
-    cout << "working" << endl;
-}
-
-//==========================================================
-//Main
-//==========================================================
-int main()
-{
-    //create an object
-    Sort temp;
-    int array[] = {1, 10, 100, 1000, 10000, 100000};
-    temp.randNumGen(array);
-    temp.selectionSort(array);
-    cout << "The sorted array is: " << endl;
-    for (int i = 0; i < arrSize; i++)
+    Sort s;
+    
+    if (first < last)
     {
-        cout << array[i] << endl;
+        //partition index
+        int p = s.partition(arr, first, last);
+
+        //separate elements into pre and post partition
+        quickSort(arr, first, p - 1);
+        quickSort(arr, p + 1, last);
+    }
+
+}
+
+//Funtion for the soft sort
+//input: l = leftmost position, m = middle, r = right most position
+//       arr = array of inputs
+//output:
+void Sort::merge(int arr[], int l ,int m, int r)
+{
+    //decalre variables
+    int i;
+    int j;
+
+    //length of the the two arrays
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    //temp arrays
+    int left[10];
+    int right[10];
+
+    //copy data to left array
+    for (i = 0; i < n1; i++)
+    {
+        left[i] = arr[l + i];
     }
     
-    return 0;
+    //copy data to right array
+    for (j = 0; j < n2; j++)
+    {
+        right[j] = arr[m + 1 + j];
+    }
+
+    //reset indexes
+    i = 0;
+    j = 0;
+    //merge index
+    int k = l;
+
+    //merge the temp arrays back into one array
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    //to get the remaining elements of left[]
+    while (i < n1)
+    {
+        arr[k] = left[i];
+        k++;
+        i++;
+        
+    }
+
+    //to get the remaining elements of right[]
+    while (j < n2)
+    {
+        arr[k] = right[j];
+        k++;
+        j++;
+    }
 }
 
+//Function for merge sort
+//input:arr[] = arr of numbers, int l = left, int r = right
+//output: sorted array and the run time
+void Sort::mergeSort(int arr[], int l, int r)
+{
+    Sort s;
+
+    if (l < r)
+    {
+
+        int m = l + (r - l) / 2;
+
+        s.mergeSort(arr, l, m);
+        s.mergeSort(arr, m + 1, r);
+
+        s.merge(arr, l, m , r);
+    }
+
+    //print array
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+
+int main()
+{
+    // Declared object
+    Sort s;
+    
+    //declare variables
+    int arr[] = {55,24,64,1,36};
+    int arraySize = sizeof(arr) / sizeof(arr[0]);
+    double time1;
+    double time2;
+    double time3;
+    
+    //use clock_t to store time
+    clock_t start, end;
+    
+    // application header
+    cout << "start of Application " << endl;
+    cout << "--------------------" << endl << endl;
+
+    //call all the functions in the class
+    s.randomIntegers(4);
+    
+    //get the time it takes by starting the clock right before you call the function
+    start = clock();
+    s.quickSort(arr, 0, arraySize - 1);
+    //and ending it right after the function is complete
+    end = clock();
+    time1 = ((double) (end - start));
+    
+    //get the time it takes by starting the clock right before you call the function
+    start = clock();
+    s.selectSort(arr, arraySize);
+    //and ending it right after the function is complete
+    end = clock();
+    time2 = ((double) (end - start));
+    
+    //get the time it takes by starting the clock right before you call the function
+    start = clock();
+    s.mergeSort(arr, 0, arraySize - 1);
+    //and ending it right after the function is complete
+    end = clock();
+    time3 = ((double) (end - start));
+    
+    
+    //print the time differences
+    cout << "\n--------------------------" << endl;
+    cout << "Runtime for Selection Sort" << endl;
+    cout << time1 << endl << endl;
+    cout << "Runtime for Quick Sort" << endl;
+    cout << time2 << endl << endl;
+    cout << "Runtime for Merge Sort" << endl;
+    cout << time3 << endl << endl;
+    
+    // application close
+    cout << "--------------------------" << endl;
+    cout << "End of Application" << endl;
+}
